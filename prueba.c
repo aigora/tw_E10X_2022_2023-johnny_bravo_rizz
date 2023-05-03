d259a9f34f00874c0895ee806689e91e6a183a68
LuisCaico <130755404+LuisCaico@users.noreply.github.com>
2023-05-03 17:21:28 +0200

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -3,7 +3,7 @@
 //aqui el menu
 int main()
 {
-  printf("elige uno de los siguientes
+  printf("elige uno de los siguientes metodos de generacion de energia: 1: Hidraulica , 2: Turbinación bombeo , 3: Nuclear ,4: Carbón
   switch
   {
-    case 1: 
+    case 1:
