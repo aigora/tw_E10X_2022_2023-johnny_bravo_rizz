213305a6be00e21f3605b11191cd5d8f7fcf3845
LuisCaico <130755404+LuisCaico@users.noreply.github.com>
2023-05-03 17:44:25 +0200

    Create prueba(luis).c

---
+++ prueba(luis).c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+  int menu;
+  printf("elige uno de los siguientes metodos de generacion de energia: 1: Hidraulica , 2: Turbinación bombeo , 3: Nuclear ,4: Carbón Fuel + Gas Motores di�sel Turbina de gas Turbina de vapor Ciclo combinado Hidroe�lica E�lica Solar fotovoltaica Solar t�rmica Otras renovables Cogeneraci�n Residuos no renovables Residuos renovables Generaci�n total");
+  scanf("%i", &menu);
