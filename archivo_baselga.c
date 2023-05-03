0add095a34e787ad9a7b32ccfe0d917f6cc5525c
ByBaselBarrabaja <132476788+ByBaselBarrabaja@users.noreply.github.com>
2023-05-03 18:42:05 +0200

    Update archivo_baselga.c

--- archivo_baselga.c
+++ archivo_baselga.c
@@ -15,14 +15,17 @@ int main()
         printf("Archivo no valido");
         exit(1);
     }
-    while (fgets(almacenFila, 1024, fp)) {
-        termino = strtok(almacenFila, ",");
-        while (termino != NULL) {
-            termino = strtok(NULL, ",");
-            if (termino != NULL) {
-                printf("%s ", termino);
+    for (int colummna = 0; columna < 5; columna++) {
+            fgets(almacenFila, 1024, fp);
+            termino = strtok(almacenFila, ",");
+            while (termino != NULL )
+            {
+                termino = strtok(NULL, ",");
+                if (termino != NULL && columna != 3)
+                {
+                    printf("%s ", termino);
+                }
             }
-        }
     }
     fclose(fp);
     return 0;
