d53410080b27184987ae2b8ff8a0df2728934679
LuisCaico <130755404+LuisCaico@users.noreply.github.com>
2023-05-03 18:20:27 +0200

    Update prueba(luis).c

--- prueba(luis).c
+++ prueba(luis).c
@@ -2,6 +2,106 @@
 
 int main()
 {
-  int menu;
-  printf("elige uno de los siguientes metodos de generacion de energia: 1: Hidraulica , 2: Turbinación bombeo , 3: Nuclear ,4: Carbón Fuel + Gas Motores di�sel Turbina de gas Turbina de vapor Ciclo combinado Hidroe�lica E�lica Solar fotovoltaica Solar t�rmica Otras renovables Cogeneraci�n Residuos no renovables Residuos renovables Generaci�n total");
+  int menu; //plantilla menu
+  printf("elige uno de los siguientes metodos de generacion de energia: 1: Hidraulica , 2: Turbinación bombeo , 3: Nuclear ,4: Carbón ,5:Fuel + Gas ,6:Motores diesel ,7:Turbina de gas ,8:Turbina de vapor ,9:Ciclo combinado ,10:Hidroeelica ,11:Eolica ,12:Solar fotovoltaica ,13: Solar termica ,14:Otras renovables ,15:Cogeneracion ,16:Residuos no renovables ,17:Residuos renovables ,18:Generacon total");
   scanf("%i", &menu);
+  switch menu
+  {
+      case 1:
+      {
+        break;
+      }
+      case 2:
+      {
+        break;
+      }
+      case 3:
+      {
+        break;
+      }
+      case 4:
+      {
+        break;
+      }
+      case 5:
+      {
+        break;
+      }
+      case 6:
+      {
+        break;
+      }
+      case 7:
+      {
+        break;
+      }
+      case 8:
+      {
+        break;
+      }
+      case 9:
+      {
+        break;
+      }
+      case 10:
+      {
+        break;
+      }
+      case 11:
+      {
+        break;
+      }
+      case 12:
+      {
+        break;
+      }
+      case 13:
+      {
+        break;
+      }
+      case 14:
+      {
+        break;
+      }
+      case 15:
+      {
+        break;
+      }
+      case 16:
+      {
+        break;
+      }
+      case 17:
+      {
+        break;
+      }
+      case 18:
+      {
+        break;
+      }
+      default:
+        printf("no es valido , vuelve a empezar");
+        break;
+  }
+  
+  //ver codigo de lectura de fichero de baselga para completar los vectores
+  
+int media(vector[n])
+{
+  int i/*valor unico*/, j=vector[0]/*sumatorio de los valores*/;
+  for(i=0; i<n ; i++)
+  {
+    j=j+vector[i];
+  }
+  int resultado=(j)/(n);/*n es el tamaño del vector , osea , el numero de valores*/
+}return resultado;
+  
+
+int maximo(vector[n])
+{
+  int resultado , i;
+  for(
+  
+  
+  
+  
