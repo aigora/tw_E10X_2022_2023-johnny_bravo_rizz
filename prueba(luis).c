946eefafccabb9aff55e77c404208a44dedd094d
LuisCaico <130755404+LuisCaico@users.noreply.github.com>
2023-05-13 18:27:37 +0200

    Update prueba(luis).c

--- prueba(luis).c
+++ prueba(luis).c
@@ -9,6 +9,7 @@ int main() //poner eñ break en el switch case
   {
       case 1:
       {
+        getVectorByNum(&datosATrabajar,1,0);
         float vector[n] , vector1[n];
         vector[n] = vector1[n]; //el abjetivo de esta linea es asignar el valor del vector seleccionado en el case al vector[n]
         int menu2;
@@ -18,6 +19,7 @@ int main() //poner eñ break en el switch case
         {
           case 1:
             {
+              media(&datosATrabajar);
               printf("la media es : %f",media(vector[n]));
               break;
             }
@@ -140,22 +142,38 @@ float media(float vector[n])
   }
   float resultado=(j)/(n);/*n es el tamaño del vector , osea , el numero de valores*/
 }return resultado
+  |
+  |
+  V
   
+  
+ float media(struct datosMatriz *datosATrabajar)
+{
+  int i;/*valor unico*/
+  float j = datosATrabajar->vectorFila[0];/*sumatorio de los valores*/
+  for(i=0; i<n ; i++)
+  {
+    j=j+datosATrabajar->vectorFila[i];
+  }
+  float resultado=(j)/(n);/*n es el tamaño del vector , osea , el numero de valores*/
+}return resultado
+  
+  datosATrabajar->numColumnas //darme el numero de columnas en un vector es el numero de datos en un vector fila
 
   
   
-float maximo(float vector[n])
+float maximo(struct datosMatriz *datosATrabajar)
 {
   int i;
   float resultado=0;
-  for(i=0 ; i<n ; i++) //alomejor habria que hacer esto con while
+  for(i=0 ; i<datosATrabajar->numColumnas ; i++) //alomejor habria que hacer esto con while
   {
-    if (resultado < vector[i])
+    if (resultado < datosATrabajar->vectorFila[i])
     {
-      resultado = vector[i]
+      resultado = datosATrabajar->vectorFila[i];
     }
   } 
-}return resultado     
+}return resultado
   
   
   
@@ -172,6 +190,22 @@ float minimo(float vector[n])
   } 
 }return resultado 
   
+ |
+ |
+ V
+  
+float minimo(struct datosMatriz *datosATrabajar)
+{
+  int i;
+  float resultado = datosATrabajar->vectorFila[0];
+  for(i=0 ; i<n ; i++) //alomejor habria que hacer esto con while
+  {
+    if (resultado > datosATrabajar->vectorFila[i])
+    {
+      resultado = datosATrabajar->vectorFila[i]
+    }
+  } 
+}return resultado 
   
   
 float varianza(float vector[n]) //revisar esto, creo que no funciona
@@ -186,7 +220,22 @@ float varianza(float vector[n]) //revisar esto, creo que no funciona
 }return resultado 
   
   
-float covarianza(float vector[n], float vector2[n])
+  |
+  |
+  V
+  float varianza(struct datosMatriz *datosATrabajar)
+{
+  int i;
+  float resultado , sum=0;
+  for(i=0 ; i<n ; i++)
+  {
+    sum = sum + ((datosATrabajar->vectorFila[i]-media(&datosATrabajar)) * (datosATrabajar->vectorFila[i]-media(&datosATrabajar)));
+  }
+  resultado = sum / datosATrabajar->numColumnas;
+}return resultado 
+  
+  
+float covarianza(float vector[n], float vector2[n]) //a terminar , és dificil juntar esto con lo de baselga pk hay dos vectores , hay que preguntarle como se hace
 {
   int i;
   for(i=0 ; i<n)
@@ -200,12 +249,37 @@ float engtot(float vector[n]) //energia total generada a lo largo del tiempo
     resultado = resultado +  vector[i];
   }
 }return resultado
+  |
+  |
+  V
+  float engtot(struct datosMatriz *datosATrabajar) //energia total generada a lo largo del tiempo
+{
+  float resultado = datosATrabajar->vectorFila[0];
+  for(i=1 , i<n , i++)
+  {
+    resultado = resultado +  datosATrabajar->vectorFila[i];
+  }
+}return resultado
   
   
-float regresion(float vector[n] , int vector2[n])//calcula la recta de regresion de una fila de datos usando la media la varianza y la covarianza(hay que hacer la covarianza aun) para usando esa recta hacer una estimacion de lo que podria ser un valor futuro
+void regresion(struct datosMatriz *datosATrabajar)//calcula la recta de regresion de una fila de datos usando la media la varianza y la covarianza(hay que hacer la covarianza aun) para usando esa recta hacer una estimacion de lo que podria ser un valor futuro
 {//en regresion hay que introducir tres datos , el primer vector (y) es el valor energético , el segundo vector son los dias o fechas en los que ocurren esos valores , las fechas se hacen como numeros enteros para facilitar los calculos .
-  float regresion, b;
-  b = (covarianza(vector[n],vector2[n]))/(varianza(vector2[n]))
+  int vectorFecha[datosATrabajar->numColumnas] 
+  float b, x;
+  //aqui calculo la covarianza poruque por ahora solo la usamos en esta funcion y es complicado hacerlo con una funcion ya que tiene dos vectores, aqui uno de esos vectores no es variable asique es mas facil
+  int i;
+  float covarianza , sum=0;
+  for(i=0 ; i<n ; i++)
+  {
+    sum = sum + ((datosATrabajar->vectorFila[i]-media(&datosATrabajar)) * (vectorFecha[i]-media(vectorFecha[])));
+  }
+  resultado = sum / datosATrabajar->numColumnas;
+  
+  
+  b = (covarianza)/(varianza(vectorFecha[n]));
+  printf("La recta de regresion tiene la forma : y= %f + %f * (x-%f)", media(datosATrabajar->vectorFila[]), b, media(vectorFecha));
+    
+  
   regresion = media(vector[n])+b*(x-media(vector2[n]))
 }
 
