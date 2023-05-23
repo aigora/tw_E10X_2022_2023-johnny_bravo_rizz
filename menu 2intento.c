5eebfa9f1981cb29c29e02b3cdfe58cb20a62704
ByBaselBarrabaja <132476788+ByBaselBarrabaja@users.noreply.github.com>
2023-05-23 14:08:58 +0200

    Update menu 2intento.c

--- menu 2intento.c
+++ menu 2intento.c
@@ -5,9 +5,10 @@ int main()
 	int menu=0, menu2;
 	while(menu!=19)
 	{
-	printf("elige uno de los siguientes metodos de generacion de energia: 1: Hidraulica , 2: Turbinación bombeo , 3: Nuclear ,4: Carbón ,5:Fuel + Gas ,6:Motores diesel ,7:Turbina de gas ,8:Turbina de vapor ,9:Ciclo combinado ,10:Hidroeelica ,11:Eolica ,12:Solar fotovoltaica ,13: Solar termica ,14:Otras renovables ,15:Cogeneracion ,16:Residuos no renovables ,17:Residuos renovables ,18:Generacon total ,19: finalizar programa\n");
-	scanf("%i", &menu);
-	getVectorByNum(&datosATrabajar,menu,0);
+	printf("What type of electricity?: ");
+	fgets(typeInput, sizeof(typeInput), stdin);
+	double vectorFila[datosATrabajar.numColumnas - 1];
+	menu = getVectorByName(&datosATrabajar, typeInput, vectorFila);
 	while(menu<1 || menu>19)
 	{
 		printf("ese numero no es valido, intenta otra vez");
