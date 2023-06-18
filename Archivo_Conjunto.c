30ec32c0aa73c6919aefb481373d767d68cc7cc9
calleruiz <131250012+calleruiz@users.noreply.github.com>
2023-06-18 17:56:58 +0200

    Update Archivo_Conjunto.c

--- Archivo_Conjunto.c
+++ Archivo_Conjunto.c
@@ -1151,7 +1151,7 @@ void grafica(struct datosMatriz *datosATrabajar, double* Vector){//funcion para 
 	double maximo, minimo;	
 	
 	int j, division;
-    // obtenerMaximoMinimo(&datosATrabajar, Vector, &maximo, &minimo);
+    obtenerMaximoMinimo(datosATrabajar, Vector, &maximo, &minimo);
     if (minimo<0){
     	minimo = minimo*(-1);
 	}
